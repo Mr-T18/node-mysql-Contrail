29d7b22ccea187f22d795a0a3adbd8852941fad4
Mr-T18 <125945725+Mr-T18@users.noreply.github.com>
2025-07-06 14:49:49 +0000

    19章おわり

--- routes/signin.js
+++ routes/signin.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const passport = require("passport")
+const passport = require("passport");
+const flash = require('connect-flash');
 
 router.get('/', function (req, res, next) {
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
+  const isAuth = req.isAuthenticated();
   res.render("signin", {
     title: "Sign in",
     isAuth: isAuth,
@@ -14,7 +14,7 @@ router.get('/', function (req, res, next) {
 router.post('/', passport.authenticate("local", {
   successRedirect: "/",
   failureRedirect: "/signin",
-  failureFlash: true,
+  // failureFlash: true, <- これなんか動かないんすよね（笑）
 }));
 
 module.exports = router;
